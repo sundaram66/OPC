273f55a0da3707a9fedfa22190f3093755b8e5b4
Seetharam <seetharam.sundaram@imaginea.com>
2016-06-02 23:10:34 +0530

    srm-621-div2-2

--- SRM 621 DIV 2/src/NumbersChallenge.java
+++ SRM 621 DIV 2/src/NumbersChallenge.java
@@ -6,7 +6,35 @@ public class NumbersChallenge {
 	
 	public int MinNumber(int[] a) {
 
-		return 0;
+		int maxSum = 0;
+		maxSum = Arrays.stream(a).sum();
+		int[] result = new int[maxSum+1];
+
+		int n = a.length;
+		int maxInt = (int) Math.pow(2.0,n);
+
+		for(int i=1;i<maxInt;i++) {
+			int sum = getSum(i,a);
+			result[sum] = 1;
+		}
+
+		for(int i=1;i<maxSum+1;i++) {
+			if(result[i]==0) return i;
+		}
+
+		return maxSum+1;
+	}
+
+	private int getSum(int value, int[] a) {
+		String s = Integer.toBinaryString(value);
+		int sum = 0,n=s.length(),n1=a.length;
+		int j=n1-n;
+		for(int i=0;i<n;i++) {
+			if(s.charAt(i)-'0' == 1)
+				sum += a[j];
+			j++;
+		}
+		return sum;
 	}
 
 }
