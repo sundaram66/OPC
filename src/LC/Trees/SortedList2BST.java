d097f1e9172c0444c0194c1c5bdf2dbf75fc4db8
Seetharam <seetharam.sundaram@imaginea.com>
2017-05-01 01:53:44 +0530

    Update SortedList2BST.java

--- src/LC/Trees/SortedList2BST.java
+++ src/LC/Trees/SortedList2BST.java
@@ -18,7 +18,7 @@
 public class Solution {
     
     static ListNode current;
-    
+    //Bottom Up
     public TreeNode sortedListToBST(ListNode head) {
         if(head == null)
             return null;
@@ -37,29 +37,45 @@ public class Solution {
         TreeNode left = getBST(st,mid-1);
         TreeNode root = new TreeNode(current.val);
         current = current.next;
-        TreeNode right = getBST(mid+1,end);
-        
+        TreeNode right = getBST(mid+1,end);        
         
         root.left = left;
         root.right = right;
         
-        return root;
-        
-        
-        
+        return root;            
     }
     
     
-    public int getLen(ListNode head) {
-        
+    public int getLen(ListNode head) {        
         int len = 0;
         while(head != null) {
             len++;
             head = head.next;
-        }
-        
+        }        
         return len;
     }
+    
+ 
+// Top Down : With Middle of Linked List.
+    
+public TreeNode sortedListToBST(ListNode head) {
+    if(head==null) return null;
+    return toBST(head,null);
+}
+public TreeNode toBST(ListNode head, ListNode tail){
+    ListNode slow = head;
+    ListNode fast = head;
+    if(head==tail) return null;
+    
+    while(fast!=tail&&fast.next!=tail){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    TreeNode thead = new TreeNode(slow.val);
+    thead.left = toBST(head,slow);
+    thead.right = toBST(slow.next,tail);
+    return thead;
+}
         
         
 }
